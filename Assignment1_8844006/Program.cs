088ef4674fd1157e85ebb5bcaa3ffff9c7529b6a
Jatin Verma <jverma4006@conestogac.on.ca>
2024-02-02 00:57:17 -0500

    Created block that will take primary choice from user for pet type selection.

--- Assignment1_8844006/Program.cs
+++ Assignment1_8844006/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Import necessary namespaces
+using System;
+using System.Threading;
+
+// PetCare class for simulation
+class PetCare
+{
+    private string petName;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Pet Simulator!");
+
+            int petTypeChoice;
+            string petType = "";
+
+            do
+            {
+                Console.WriteLine("\nChoose the type of pet:");
+                Console.WriteLine("1. Cat");
+                Console.WriteLine("2. Dog");
+                Console.WriteLine("3. Rabbit");
+                Console.WriteLine("4. Hamster");
+
+                Console.Write("Enter the number for your desired pet type: ");
+
+                if (!int.TryParse(Console.ReadLine(), out petTypeChoice) || petTypeChoice < 1 || petTypeChoice > 4)
+                {
+                    Console.WriteLine("\nError: Invalid choice. Please enter a number for the given choices.");
+                    continue;
+                }
+
+                switch (petTypeChoice)
+                {
+                    case 1:
+                        petType = "Cat";
+                        break;
+                    case 2:
+                        petType = "Dog";
+                        break;
+                    case 3:
+                        petType = "Rabbit";
+                        break;
+                    case 4:
+                        petType = "Hamster";
+                        break;
+                    default:
+                        Console.WriteLine("Error: Invalid choice. Please enter a number for the given choices.");
+                        break;
+                }
+
+            } while (string.IsNullOrEmpty(petType));
+
+            PetCare userPet = new PetCare();
+            Console.Write($"Enter your {petType}'s name: ");
+            //userPet.PetName = Console.ReadLine();
+
+
+        }
+    }
+}
